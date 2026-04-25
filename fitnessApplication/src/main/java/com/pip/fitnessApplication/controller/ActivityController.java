e52db2cc878c8803108de65f71919591a0658531
francesco <francesco-andrei.fotache@student.tuiasi.ro>
2026-04-25 20:10:20 +0200

    PostAPI

---
+++ fitnessApplication/src/main/java/com/pip/fitnessApplication/controller/ActivityController.java
@@ -0,0 +1,27 @@
+package com.pip.fitnessApplication.controller;
+
+import com.pip.fitnessApplication.dto.ActivityDto;
+import com.pip.fitnessApplication.services.activity.ActivityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class ActivityController {
+    private final ActivityService activityService;
+
+    @PostMapping("/activity")
+    public ResponseEntity<?> postActivity(@RequestBody ActivityDto dto){
+        ActivityDto createActivity = activityService.postActivity(dto);
+
+        if(createActivity != null){
+            return  ResponseEntity.status(HttpStatus.CREATED).body(createActivity);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+        }
+    }
+}
---
+++ fitnessApplication/src/main/java/com/pip/fitnessApplication/services/activity/ActivityServiceImplementation.java
@@ -0,0 +1,24 @@
+package com.pip.fitnessApplication.services.activity;
+
+import com.pip.fitnessApplication.dto.ActivityDto;
+import com.pip.fitnessApplication.entity.Activity;
+import com.pip.fitnessApplication.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityServiceImplementation implements ActivityService{
+    private final ActivityRepository activityRepository; // conexiunea cu baza de date CRUD
+
+    public ActivityDto postActivity(ActivityDto dto){
+        Activity activity = new Activity();
+
+        activity.setDate(dto.getDate());
+        activity.setSteps(dto.getSteps());
+        activity.setDistance(dto.getDistance());
+        activity.setCaloriesBurned(dto.getCaloriesBurned());
+
+        return activityRepository.save(activity).getActivityDto();
+    }
+}
