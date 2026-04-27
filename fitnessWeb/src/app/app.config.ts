dae9980be2bc2a0862089a0a8e48492c56b8a663
Robert <robertpopa015@gmail.com>
2026-04-27 13:53:43 +0300

    Bug limba rezolvat

--- fitnessWeb/src/app/app.config.ts
+++ fitnessWeb/src/app/app.config.ts
@@ -1,25 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, NgModule } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideBrowserGlobalErrorListeners } from '@angular/core';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+// --- Setările pentru Limba Engleză în NG-ZORRO ---
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-
 registerLocaleData(en);
-
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
-@NgModule({
-  providers: [provideHttpClient(withInterceptorsFromDi())],
-})
-export class AppModule {}
+// --------------------------------------------------
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideBrowserGlobalErrorListeners(), // L-am pus la loc pe cel din codul tău original
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptorsFromDi()), // Mutat aici din vechiul modul
+    provideAnimationsAsync(), // Pentru calendar
+    provideNzI18n(en_US)      // Pentru limba engleză
   ],
 };
